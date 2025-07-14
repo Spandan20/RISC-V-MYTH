0bb8850a4965eca8dc7766896684670849ed9cce
Spandan20 <spandanbusiness@outlook.com>
2025-07-14 19:36:11 +0530

    Update calculator to 2-cycle calculator

--- Day3_5/calculator.v
+++ Day3_5/calculator.v
@@ -1,34 +1,54 @@
-\m5_TLV_version 1d: tl-x.org
-\m5
-   
-   // ============================================
-   // Welcome, new visitors! Try the "Learn" menu.
-   // ============================================
-   
-   //use(m5-1.0)   /// uncomment to use M5 macro library.
+\m4_TLV_version 1d: tl-x.org
 \SV
-   // Macro providing required top-level module definition, random
-   // stimulus support, and Verilator config.
-   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
+   // This code can be found in: https://github.com/stevehoover/RISC-V_MYTH_Workshop
+   
+   m4_include_lib(['https://raw.githubusercontent.com/stevehoover/RISC-V_MYTH_Workshop/ecba3769fff373ef6b8f66b3347e8940c859792d/tlv_lib/calculator_shell_lib.tlv'])
+
+\SV
+   m4_makerchip_module   // (Expanded in Nav-TLV pane.)
+
 \TLV
-   $reset = *reset;
+   |calc
+      @0
+         $reset = *reset;
+         
+         
+      @1
+         
+         $val1[31:0] = >>2$out;
+         $val2[31:0] = $rand2[3:0];
+
+         $sum[31:0] = $val1[31:0] + $val2[31:0];
+         $diff[31:0] = $val1[31:0] - $val2[31:0];
+         $prod[31:0] = $val1[31:0] * $val2[31:0];
+         $quot[31:0] = $val1[31:0] / $val2[31:0];
+         
+         $valid = $reset ? 1'b0 : (>>1$valid + 1);
+      @2
+         $op[1:0] = 'x;
+         $out[31:0] = $reset || !$valid ? 32'b0 :
+                      ($op == 0) ? $sum :
+                      ($op == 1) ? $diff :
+                      ($op == 2) ? $prod : $quot;
+         
+         
+
+      // Macro instantiations for calculator visualization(disabled by default).
+      // Uncomment to enable visualisation, and also,
+      // NOTE: If visualization is enabled, $op must be defined to the proper width using the expression below.
+      //       (Any signals other than $rand1, $rand2 that are not explicitly assigned will result in strange errors.)
+      //       You can, however, safely use these specific random signals as described in the videos:
+      //  o $rand1[3:0]
+      //  o $rand2[3:0]
+      //  o $op[x:0]
+      
+   //m4+cal_viz(@3) // Arg: Pipeline stage represented by viz, should be atleast equal to last stage of CALCULATOR logic.
+
    
-   $op[1:0] = $rand1[1:0];
-   $val1[31:0] = $reset ? 0 : >>1$out;
-   $val2[31:0] = $rand2[3:0];
-   
-   $sum[31:0] = $val1[31:0] + $val2[31:0];
-   $diff[31:0] = $val1[31:0] - $val2[31:0];
-   $prod[31:0] = $val1[31:0] * $val2[31:0];
-   $quot[31:0] = $val1[31:0] / $val2[31:0];
-   
-   $out[31:0] = $reset ? 0 :
-                ($op == 0) ? $sum :
-                ($op == 1) ? $diff :
-                ($op == 2) ? $prod : $quot;
-   
-   // Assert these to end simulation (before the cycle limit).
+   // Assert these to end simulation (before Makerchip cycle limit).
    *passed = *cyc_cnt > 40;
    *failed = 1'b0;
+   
+
 \SV
    endmodule
